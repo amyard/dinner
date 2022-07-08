bbfdc2f2c10c44d7e3f9d1f3c084d82275b29c26
amyard <maksymturenko@gmail.com>
2022-07-08 23:36:42 +0300

    error handler - add ProblemDetails into ErrorHandlingFilterAttribute

--- BuyBerDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
+++ BuyBerDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace BuyBerDinner.Api.Filters;
@@ -8,12 +9,16 @@ public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
     public override void OnException(ExceptionContext context)
     {
         var exception = context.Exception;
-        var errorResult = new {error = "An error occurred while processing your request."};
-        
-        context.Result = new ObjectResult(errorResult)
+        var problemDetails = new ProblemDetails
         {
-            StatusCode = StatusCodes.Status500InternalServerError
+            Instance = context.HttpContext.Request.Path,
+            Status = (int)HttpStatusCode.InternalServerError,
+            Detail = exception.Message,
+            Title = "An error occurred while processing your request.",
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1"
         };
+
+        context.Result = new ObjectResult(problemDetails);
         context.ExceptionHandled = true;
     }
 }
