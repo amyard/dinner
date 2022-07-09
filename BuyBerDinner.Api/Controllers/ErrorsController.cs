a5f03b9f113bec8ed240cf952e51c750885c24c4
amyard <maksymturenko@gmail.com>
2022-07-09 14:30:06 +0300

    error handler - add handling through error controller.

---
+++ BuyBerDinner.Api/Controllers/ErrorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuyBerDinner.Api.Controllers;
+
+public class ErrorsController : ControllerBase
+{
+    [Route("error")]
+    public IActionResult Error()
+    {
+        Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+        
+        // ProblemDetail.Type - type url will change depends on status code.
+        return Problem(title: exception?.Message, statusCode: 400);
+    }
+}
