652a8cac61fe1075b27946f010191c313e732986
Maksym <maksymturenko@gmail.com>
2022-07-11 16:34:02 +0300

    add invalid password exception.

---
+++ BuyBerDinner.Application/Common/Errors/InvalidPasswordException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace BuyBerDinner.Application.Common.Errors;
+
+public class InvalidPasswordException : Exception, IServiceException
+{
+    public HttpStatusCode StatusCode => HttpStatusCode.Conflict;
+    public string ErrorMessage => "Invalid password.";
+}
--- BuyBerDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuyBerDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -20,11 +20,11 @@ public class AuthenticationService : IAuthenticationService
     {
         // 1. validate the user exists
         if (_userRepository.GetUserByEmail(email) is not User user)
-            throw new Exception("User with given email already exists.");
-        
+            throw new DuplicateEmailException();
+
         // 2. validate the password is correct
         if (user.Password != password)
-            throw new Exception("Invalid password.");
+            throw new InvalidPasswordException();
         
         // 3. create jwt token
         var token = _jwtTokenGenerator.GenerateToken(user);
@@ -36,8 +36,8 @@ public class AuthenticationService : IAuthenticationService
     {
         // 1. validate the user doesn't exists.
         if (_userRepository.GetUserByEmail(email) is not null)
-            throw new DuplicateEmailException();
-        
+            throw new Exception("User with given email already exists.");
+            
         // 2. create user & persist to DB
         var user = new User(firstName, lastName, email, password);
         _userRepository.Add(user);
